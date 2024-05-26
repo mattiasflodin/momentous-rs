7410fc9c26cbd14166b391468f66a93f0be13a86
Mattias Flodin <mattias@codepentry.com>
2024-05-26 13:10:45 +0200

    Get minute

--- src/iso8601/mod.rs
+++ src/iso8601/mod.rs
@@ -10,4 +10,6 @@ mod util;
 
 const SECONDS_PER_DAY: u32 = 86_400;
 const SECONDS_PER_HOUR: u16 = 3_600;
+const SECONDS_PER_MINUTE: u8 = 60;
 const HOURS_PER_DAY: u8 = 24;
+const MINUTES_PER_HOUR: u8 = 60;
